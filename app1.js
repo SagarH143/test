d952775ae0f46e34248a67fb03d8a66cbdee5d92
SagarH143 <s.hunshayal@hnak.com>
2021-03-09 14:16:05 +0530

    sagar

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 console.log('hi sagar welcome to github.com');
 const sagar=()=>{
-    console.log('yes we are ');
+    console.log('yes we are sagar ');
     console.log('hi i am  sagar ');
   console.log('hi');
   console.log('hello');
