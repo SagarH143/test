0db41a79577325157a618e273b4dfa417700a3d6
SagarH143 <80143681+SagarH143@users.noreply.github.com>
2021-03-09 14:32:27 +0530

    prajesh is added

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 console.log('hi sagar welcome to github.com');
 const sagar=()=>{
-    console.log('yes we are in other team');
+    console.log('yes we are in other team prajesh');
     console.log('hi i am  sagar ');
   console.log('hi');
   console.log('hello');
