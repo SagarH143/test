86960d260a00c9a5d5f1dc4cf91d3aa6b4af9e8f
SagarH143 <80143681+SagarH143@users.noreply.github.com>
2021-03-09 14:17:04 +0530

    added in team

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 console.log('hi sagar welcome to github.com');
 const sagar=()=>{
-    console.log('yes we are ');
+    console.log('yes we are in other team');
     console.log('hi i am  sagar ');
   console.log('hi');
   console.log('hello');
